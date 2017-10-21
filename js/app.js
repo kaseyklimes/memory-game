75c5294121c9954333f697a47dfc49d1fbf2d937
Kasey Klimes <kaseyklimes@me.com>
2017-10-21 11:03:09 -0700

    add clicked cards to open cards list

--- js/app.js
+++ js/app.js
@@ -48,3 +48,20 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ openCards = []
+
+ let addToOpen = function() {
+   $('.card').click(function() {
+     $(this).addClass('match');
+     if (openCards.length < 2) {
+       openCards.push($(this).children()[0]);
+     } else {
+       openCards = [];
+       openCards.push($(this).children()[0]);
+
+     };
+   });
+ };
+
+ addToOpen();
